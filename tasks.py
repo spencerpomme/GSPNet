19e3a87ab1b6eb1536bb3dd3143a28cd3cbc3bb6
spencerpomme <spencerpomme@live.com>
2019-04-01 10:34:53 +0800

    Delete tasks.py

--- tasks.py
+++
@@ -1,8 +0,0 @@
-
-
-if __name__ == '__main__':
-
-    urls = [] # a container of urls, here use python list
-    results = para_get_info(urls) # save result strings to a container
-    write_to_db
-    
