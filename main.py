0d3daa9f9331ac32a8ce8d0f63f65e759fb3e8ca
spencerpomme <spencerpomme@live.com>
2019-04-05 15:30:12 +0800

    timeslice module's performance is 30 times faster in average now

--- main.py
+++ main.py
@@ -24,39 +24,40 @@ if __name__ == '__main__':
 
 
     # taxi = source.DatabaseSource('cleaned_small_yellow_2017_full', ('2017-01-01 00:00:00', '2017-02-01 00:00:00'))
-    taxi = source.DatabaseSource('cleaned_small_yellow_2017_02', ('2017-02-01 00:00:00', '2017-03-01 00:00:00'))
+    taxi = source.DatabaseSource('cleaned_small_yellow_2017_full', ('2017-01-01 00:00:00', '2018-01-01 00:00:00'))
     taxi.load()
 
     tables = taxi.table_pool
     sub_ranges = taxi.sub_ranges
     tb_size = len(tables)
-    workers = []
 
+    total_start = time.time()
+    for freq in ['30min', '20min', '15min', '10min', '5min']:
+        # multi processing generate data
+        
+        workers = []
+        for k in tables.keys():
 
-    # multi processing generate data
-    for k in tables.keys():
+            # wp = worker.Worker(k, tables[k], rule.TimeSlice(*list(map(str, sub_ranges[k])), freq='10min'), 'full_year_10min', True)
+            wp = worker.Worker(k, tables[k], rule.TimeSlice(*list(map(str, sub_ranges[k])), freq=freq), f'tensor_dataset/full_year_{freq}', True)
+            workers.append(wp)
 
-        # wp = worker.Worker(k, tables[k], rule.TimeSlice(*list(map(str, sub_ranges[k])), freq='10min'), 'full_year_10min', True)
-        wp = worker.Worker(k, tables[k], rule.TimeSlice(*list(map(str, sub_ranges[k])), freq='15min'), 'speed_test', True)
-        workers.append(wp)
+        print(f'Start generating tensors with freq {freq}at {time.ctime()}\n')
+        start = time.time()
 
-    print(f'Start generating tensors at {time.ctime()}')
-    start = time.time()
+        ############################### code start here ##############################
+        
+        # create a process pool
+        pn = cpu_count()
+        print(f'Creating pool with {pn} processes.')
+        
+        with Pool(pn) as pool:
+            pool.map(f, workers)
 
-    ############################### code start here ##############################
-    
-    # create a process pool
-    pn = cpu_count()
-    print(f'Creating pool with {pn} processes.')
-    
-    with Pool(pn) as pool:
-        pool.map(f, workers)
-
-        pool.close()
-        pool.join()
-
-    # for worker in workers:
-    #     worker.generate()
-    
-    end = time.time()
-    print(f'Generation finished at {time.ctime()} in {end-start :2f} seconds.')
+            pool.close()
+            pool.join()
+        
+        end = time.time()
+        print(f'Generation of tensors with freq {freq} finished at {time.ctime()} in {end-start :2f} seconds.\n\n')
+    total_end = time.time()
+    print(f'Entire process of generating tensor datas ended in {total_end-total_start :2f} seconds.')
