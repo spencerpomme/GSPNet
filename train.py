57dbfdf92e7275690bc3e0e954436e8fb018fe25
spencerpomme <spencerpomme@live.com>
2019-04-07 21:10:56 +0800

    create train.py

--- train.py
+++ train.py
@@ -34,144 +34,94 @@ from torch.utils.data import TensorDataset, DataLoader
 from glob import glob, iglob
 
 
-def train(net, data, epochs=10, batch_size=10, seq_length=50, lr=0.001, clip=5, val_frac=0.1, print_every=10):
-    ''' Training a network 
-    
-        Arguments
-        ---------
-        
-        net: CharRNN network
-        data: text data to train the network
-        epochs: Number of epochs to train
-        batch_size: Number of mini-sequences per mini-batch, aka batch size
-        seq_length: Number of character steps per mini-batch
-        lr: learning rate
-        clip: gradient clipping
-        val_frac: Fraction of data to hold out for validation
-        print_every: Number of steps for printing training and validation loss
-    
-    '''
-    net.train()
-    
-    opt = torch.optim.Adam(net.parameters(), lr=lr)
-    criterion = nn.CrossEntropyLoss()
-    
-    # create training and validation data
-    val_idx = int(len(data)*(1-val_frac))
-    data, val_data = data[:val_idx], data[val_idx:]
-    
-    if(train_on_gpu):
-        net.cuda()
-    
-    counter = 0
-    n_chars = len(net.chars)
-    for e in range(epochs):
-        # initialize hidden state
-        h = net.init_hidden(batch_size)
-        
-        for x, y in get_batches(data, batch_size, seq_length):
-            counter += 1
-            
-            # One-hot encode our data and make them Torch tensors
-            x = one_hot_encode(x, n_chars)
-            inputs, targets = torch.from_numpy(x), torch.from_numpy(y)
-            
-            if(train_on_gpu):
-                inputs, targets = inputs.cuda(), targets.cuda()
-
-            # Creating new variables for the hidden state, otherwise
-            # we'd backprop through the entire training history
-            h = tuple([each.data for each in h])
-
-            # zero accumulated gradients
-            net.zero_grad()
-            
-            # get the output from the model
-            output, h = net(inputs, h)
-            
-            # calculate the loss and perform backprop
-            loss = criterion(output, targets.view(batch_size*seq_length))
-            loss.backward()
-            # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
-            nn.utils.clip_grad_norm_(net.parameters(), clip)
-            opt.step()
-            
-            # loss stats
-            if counter % print_every == 0:
-                # Get validation loss
-                val_h = net.init_hidden(batch_size)
-                val_losses = []
-                net.eval()
-                for x, y in get_batches(val_data, batch_size, seq_length):
-                    # One-hot encode our data and make them Torch tensors
-                    x = one_hot_encode(x, n_chars)
-                    x, y = torch.from_numpy(x), torch.from_numpy(y)
-                    
-                    # Creating new variables for the hidden state, otherwise
-                    # we'd backprop through the entire training history
-                    val_h = tuple([each.data for each in val_h])
-                    
-                    inputs, targets = x, y
-                    if(train_on_gpu):
-                        inputs, targets = inputs.cuda(), targets.cuda()
-
-                    output, val_h = net(inputs, val_h)
-                    val_loss = criterion(output, targets.view(batch_size*seq_length))
-                
-                    val_losses.append(val_loss.item())
-                
-                net.train() # reset to train mode after iterationg through validation data
-                
-                print("Epoch: {}/{}...".format(e+1, epochs),
-                      "Step: {}...".format(counter),
-                      "Loss: {:.4f}...".format(loss.item()),
-                      "Val Loss: {:.4f}".format(np.mean(val_losses)))
+# helper functions
+def save_model(filename, decoder):
+    save_filename = os.path.splitext(os.path.basename(filename))[0] + '.pt'
+    torch.save(decoder, save_filename)
 
 
-def forward_back_prop(rnn, optimizer, criterion, inp, target, hidden, clip):
+def load_model(filename):
+    save_filename = os.path.splitext(os.path.basename(filename))[0] + '.pt'
+    return torch.load(save_filename)
+
+
+def forward_back_prop(model, optimizer, criterion, inp, target, hidden, clip):
     """
-    Forward and backward propagation on the neural network
-    :param decoder: The PyTorch Module that holds the neural network
-    :param decoder_optimizer: The PyTorch optimizer for the neural network
-    :param criterion: The PyTorch loss function
-    :param inp: A batch of input to the neural network
-    :param target: The target output for the batch of input
-    :return: The loss and the latest hidden state Tensor
-    """    
-    # TODO: Implement Function
+    Forward and backward propagation on the neural network.
 
+    Args:
+        model:     The PyTorch Module that holds the neural network
+        optimizer: The PyTorch optimizer for the neural network
+        criterion: The PyTorch loss function
+        inp:       A batch of input to the neural network
+        target:    The target output for the batch of input
+        hidden:    Hidden state
+        clip:      Clip the overly large gradient
+
+    Returns:
+        The loss and the latest hidden state Tensor
+    """
     # Creating new variables for the hidden state, otherwise
     # we'd backprop through the entire training history
     h = tuple([each.data for each in hidden])
-        
+
     # zero accumulated gradients
-    rnn.zero_grad()
-    
+    model.zero_grad()
+
     # print(f'input shape: {inp}, target shape: {target}')
     # get the output from the model
-    output, h = rnn(inp, h)
+    output, h = model(inp, h)
 
     # perform backpropagation and optimization
     # calculate the loss and perform backprop
     loss = criterion(output, target)
-    
     loss.backward()
+
     # 'clip_grad_norm' helps prevent the exploding gradient problem in RNNs / LSTMs
-    nn.utils.clip_grad_norm_(rnn.parameters(), clip)
+    nn.utils.clip_grad_norm_(model.parameters(), clip)
     optimizer.step()
+
     # return the loss over a batch and the hidden state produced by our model
     return loss.item(), h
 
 
-def train_rnn(rnn, batch_size, optimizer, criterion, n_epochs, clip=5, show_every_n_batches=100):
-    batch_losses = []
+def train_rnn(model, batch_size, optimizer, criterion, n_epochs,
+              train_loader, valid_loader, clip=5, stop_criterion=5,
+              show_every_n_batches=100):
+    '''
+    Train a RNN model with the given hyperparameters.
 
-    rnn.train()
+    Args:
+        model:
+        batch_size:
+        optimizer:
+        criterion:
+        n_epochs:
+        train_loader:
+        valid_loader:
+        clip:
+        show_every_batches:
+
+    Returns:
+        A trained model
+    '''
+    # clear cache
+    torch.cuda.empty_cache()
+    # start timing
+    start = time.time()
+    print(f'Training started at {time.ctime()}')
+    # validation constants
+    early_stop_count = 0
+    valid_loss_min = np.inf
+
+    train_losses = []
+
+    model.train()
 
     print("Training for %d epoch(s)..." % n_epochs)
     for epoch_i in range(1, n_epochs + 1):
 
-        hidden = rnn.init_hidden(batch_size)
+        hidden = model.init_hidden(batch_size)
 
         for batch_i, (inputs, labels) in enumerate(train_loader, 1):
 
@@ -184,15 +134,56 @@ def train_rnn(rnn, batch_size, optimizer, criterion, n_epochs, clip=5, show_ever
             # print(f'inputs shape: {inputs.shape} labels shape: {labels.shape}')
             # print(f'inputs dtype: {inputs[0][0][0].dtype} label shape: {labels[0][0].dtype}')
             inputs, labels = inputs.cuda(), labels.cuda()
-            loss, hidden = forward_back_prop(
-                rnn, optimizer, criterion, inputs, labels, hidden, clip)
-            # record loss
-            batch_losses.append(loss)
-            if (batch_i % show_every_n_batches == 0):
-                # printing loss stats
-                print('Epoch: {:>4}/{:<4}  Loss: {}\n'.format(epoch_i,
-                                                              n_epochs, np.average(batch_losses)))
-                batch_losses = []
 
-    # returns a trained rnn
-    return rnn
+            loss, hidden = forward_back_prop(
+                model, optimizer, criterion, inputs, labels, hidden, clip
+            )
+
+            # record loss
+            train_losses.append(loss)
+
+            # print loss every show_every_n_batches batches
+            # including validation loss
+            if batch_i % show_every_n_batches == 0:
+                # get validation loss
+                val_h = model.init_hidden(batch_size)
+                valid_losses = []
+
+                # switch to validation mode
+                model.eval()
+
+                for v_inputs, v_labels in valid_loader:
+
+                    v_inputs, v_labels = v_inputs.cuda(), v_labels.cuda()
+
+                    # Creating new variables for the hidden state, otherwise
+                    # we'd backprop through the entire training history
+                    val_h = tuple([each.data for each in val_h])
+
+                    v_output, val_h = model(v_inputs, val_h)
+                    val_loss = criterion(v_output, v_labels)
+
+                    valid_losses.append(val_loss.item())
+
+                model.train()
+                avg_val_loss = np.mean(valid_losses)
+                # printing loss stats
+                print(
+                    f'Epoch: {epoch_i:>4}/{n_epochs:<4}  Loss: {np.mean(batch_losses)}  Val Loss {avg_val_loss}')
+
+                # decide whether to save model or not:
+                if avg_val_loss < valid_loss_min:
+                    print(
+                        f'Valid Loss {valid_loss_min:4f} -> {avg_val_loss:4f}. Saving...')
+                    torch.save(model.state_dict(),
+                               './trained_models/best_model.pt')
+                train_losses = []
+                valid_losses = []
+
+    # returns a trained model
+    return model
+
+
+if __name__ == '__main__':
+
+    print('In main of train.py')
