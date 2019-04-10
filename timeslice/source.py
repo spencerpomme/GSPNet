1df1fa7bd553ee693ee0da4e3429a9596dc8bcb9
spencerpomme <spencerpomme@live.com>
2019-04-10 23:18:09 +0800

    Update source.py

--- timeslice/source.py
+++ timeslice/source.py
@@ -18,7 +18,8 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-##============================================================================##
+Data source connector, front-end of worker.py
+A part of GSPNet project.
 
 '''
 import numpy as np
