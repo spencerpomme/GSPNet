26434ca9ab3577f30ce4c9bf8ede3e18e7bf332d
spencerpomme <spencerpomme@live.com>
2019-04-10 23:18:16 +0800

    Update train.py

--- train.py
+++ train.py
@@ -18,9 +18,11 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-##============================================================================##
+Training methods and customized datset classes defined here.
+A part of GSPNet project.
 
 '''
+
 import numpy as np
 import pandas as pd
 import pickle as pkl
