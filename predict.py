0f652434d1c3a7b078546729b1136089c9820878
spencerpomme <spencerpomme@live.com>
2019-04-12 22:17:17 +0800

    Create predict.py

---
+++ predict.py
@@ -0,0 +1,104 @@
+import torch.nn.functional as F
+'''
+Copyright <2019> <COPYRIGHT Pingcheng Zhang>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Generate future trafic states.
+A part of GSPNet project.
+
+'''
+import numpy as np
+import pandas as pd
+import pickle as pkl
+import matplotlib.pyplot as plt
+import os
+import re
+import time
+import torch
+
+from torch import nn, optim
+from torch.utils import data
+from torch.utils.data import TensorDataset, DataLoader
+from glob import iglob, glob
+from matplotlib import pyplot as plt
+from matplotlib.legend_handler import HandlerLine2D
+from tqdm import tqdm
+
+
+# TODO: finish this class
+def generate(model, init_state, predict_len=4):
+    '''
+    Generate text using the neural network
+
+    Args:
+        model:        The PyTorch Module that holds the trained neural network
+        init_state:   The word id to start the first prediction
+        predict_len:  The length of text to generate
+    Returns:
+        The generated traffic states
+    '''
+    rnn.eval()
+
+    # create a sequence (batch_size=1) with the prime_id
+    current_seq = np.full((1, sequence_length), pad_value)
+    current_seq[-1][-1] = prime_id
+    predicted = [int_to_vocab[prime_id]]
+
+    for _ in range(predict_len):
+        if train_on_gpu:
+            current_seq = torch.LongTensor(current_seq).cuda()
+        else:
+            current_seq = torch.LongTensor(current_seq)
+
+        # initialize the hidden state
+        if train_on_multi_gpus:
+            hidden = rnn.module.init_hidden(current_seq.size(0))
+        else:
+            hidden = rnn.init_hidden(current_seq.size(0))
+
+        # get the output of the rnn
+        output, _ = rnn(current_seq, hidden)
+
+        # get the next word probabilities
+        p = F.softmax(output, dim=1).data
+        if(train_on_gpu):
+            p = p.cpu()  # move to cpu
+
+        # use top_k sampling to get the index of the next word
+        top_k = 5
+        p, top_i = p.topk(top_k)
+        top_i = top_i.numpy().squeeze()
+
+        # select the likely next word index with some element of randomness
+        p = p.numpy().squeeze()
+        word_i = np.random.choice(top_i, p=p/p.sum())
+
+        # retrieve that word from the dictionary
+        word = int_to_vocab[word_i]
+        predicted.append(word)
+
+        # the generated word becomes the next "current sequence" and the cycle can continue
+        if train_on_multi_gpus or train_on_gpu:
+            current_seq = current_seq.cpu()
+        current_seq = np.roll(current_seq, -1, 1)
+        current_seq[-1][-1] = word_i
+
+    gen_sentences = ' '.join(predicted)
+
+    return gen_sentences
