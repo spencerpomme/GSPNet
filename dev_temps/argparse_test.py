4d42cc7873bccfb400477a43216282d10b09bfdf
spencerpomme <spencerpomme@live.com>
2019-04-13 13:54:22 +0800

    Update argparse_test.py

--- dev_temps/argparse_test.py
+++ dev_temps/argparse_test.py
@@ -1,11 +1,16 @@
 import argparse
-
+import sys
 
 parser = argparse.ArgumentParser(description='A sample test program for argparse.')
 parser.add_argument('-a', action='store_true', default=False)
 parser.add_argument('-b', action='store', dest='b')
 parser.add_argument('-c', action='store', dest='c')
 
-args = parser.parse_args()
 
-print(args.a, args.b, args.c)
+def main(*args):
+    for arg in args:
+        print(arg)
+
+if __name__ == '__main__':
+
+    main(*sys.argv)
