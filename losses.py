59395e32e8b1b3135bf37402e5f1bf559d319925
spencerpomme <spencerpomme@live.com>
2019-04-29 09:38:22 +0800

    Update losses.py

--- losses.py
+++ losses.py
@@ -102,6 +102,28 @@ def vae_loss(recon_x, x, mu, logvar):
     return BCE + KLD, BCE, KLD
 
 
-# sparse autoencoder loss
-def sparse_encoder_loss(target, label):
-    pass
+class L1Penality(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, input, l1weight):
+        """
+        In the forward pass we receive a Tensor containing the input and return
+        a Tensor containing the output. ctx is a context object that can be used
+        to stash information for backward computation. You can cache arbitrary
+        objects for use in the backward pass using the ctx.save_for_backward method.
+        """
+        ctx.save_for_backward(input)
+        ctx.l1weight = l1weight
+        return input
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        """
+        In the backward pass we receive a Tensor containing the gradient of the loss
+        with respect to the output, and we need to compute the gradient of the loss
+        with respect to the input.
+        """
+        input, = ctx.saved_tensors
+        grad_input = input.clone().sign().mul(ctx.l1weight)
+        grad_input += grad_output
+        return grad_input, None
