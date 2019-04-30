2066f091a208473a66fef1fb6a93e7a287d24655
spencerpomme <spencerpomme@live.com>
2019-04-30 16:56:46 +0800

    add weight init method

--- models.py
+++ models.py
@@ -504,14 +504,32 @@ def deconv(in_channels, out_channels, kernel_size, stride=2, padding=1, batch_no
 
 
 def scale(x, feature_range=(-1, 1)):
-    ''' Scale takes in an image x and returns that image, scaled
+    '''
+    Scale takes in an image x and returns that image, scaled
        with a feature_range of pixel values from -1 to 1. 
-       This function assumes that the input x is already scaled from 0-1.'''
+       This function assumes that the input x is already scaled from 0-1.
+    '''
     # assume x is scaled to (0, 1)
     # scale to feature_range and return scaled x
     min, max = feature_range
     x = x * (max - min) + min
     return x
+
+
+def weights_init_normal(m):
+    """
+    Applies initial weights to certain layers in a model .
+    The weights are taken from a normal distribution 
+    with mean = 0, std dev = 0.02.
+    :param m: A module or layer in a network    
+    """
+    # classname will be something like:
+    # `Conv`, `BatchNorm2d`, `Linear`, etc.
+    classname = m.__class__.__name__
+
+    if classname.find('Conv') != -1 or classname.find('Linear') != -1:
+        nn.init.normal_(m.weight.data, mean=0, std=0.02)
+        # nn.init.constant_(m.weight.data, 0.45)
 
 
 class Discriminator(nn.Module):
