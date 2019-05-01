7fd51a1b137df0e32d3612977962b26a0b5dd578
spencerpomme <spencerpomme@live.com>
2019-05-01 22:29:23 +0800

    Update sanity_check.py

--- sanity_check.py
+++ sanity_check.py
@@ -43,6 +43,13 @@ from matplotlib.legend_handler import HandlerLine2D
 from tqdm import tqdm
 from torchvision import datasets, transforms
 
+# import models, loss functions and datasets
+import models
+from models import *
+from losses import *
+from datasets import *
+from train import *
+
 
 def shrink_large_img(img_path):
     bad_file_list = []
@@ -64,26 +71,88 @@ if __name__ == '__main__':
     # Hyperparameters:
     batch_size = 256
     num_workers = 0
+    learning_rate = 0.1
+    epochs = 20
+    mode = 'pnf'
+    save_dir = 'trained_models'
+    dest = 'autoencoder_test/sanitycheck'
 
-    # data root
-    data_dir = 'data/dogcheck'
-
-    # preprocess images: shrink too big images:
-    dog_files = np.array(glob(data_dir + '/*.jpg'))
-
-    for path in dog_files:
-        shrink_large_img(path)
+    hyps = {
+        'is': 111,
+        'os': 111,
+        'mn': 'sanitycheck',
+        'hd': 111,
+        'bs': batch_size,
+        'lr': learning_rate,
+        'md': mode
+    }
 
     # training data transforms
-    t = transforms.Compose([transforms.Resize(100), transforms.CenterCrop(69),
+    t = transforms.Compose([transforms.Resize(81), transforms.CenterCrop(69),
                             transforms.ToTensor(),
                             transforms.Normalize([0.485, 0.456, 0.406],
                                                  [0.229, 0.224, 0.225])
                             ])
 
-    # Pass transforms in here, then run the next cell to see how the
-    #  transforms look
-    data = datasets.ImageFolder(data_dir, transform=t)
+    data = datasets.CIFAR10(
+        'data/sanity_check/cifar10', train=True, transform=t, download=True)
 
-    loader = torch.utils.data.DataLoader(data, batch_size=batch_size,
-                                         num_workers=num_workers, shuffle=True)
+    # split dataset for training and validation
+    num_train = len(data)
+    indices = list(range(num_train))
+    split = int(np.floor(0.8 * num_train))  # hard coded to 0.8
+
+    train_idx, valid_idx = indices[split:], indices[:split]
+    train_sampler = SequentialSampler(train_idx)
+    valid_sampler = SequentialSampler(valid_idx)
+
+    train_loader = DataLoader(data, sampler=train_sampler,
+                              batch_size=batch_size, num_workers=0, drop_last=True)
+
+    valid_loader = DataLoader(data, sampler=valid_sampler,
+                              batch_size=batch_size, num_workers=0, drop_last=True)
+
+    model = ConvAutoEncoderShallow(69*69*3, mode='pnf')
+    model = model.to('cuda:1')
+
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+    criterion = nn.MSELoss()
+
+    trained_model = train_encoder(model, optimizer, criterion, epochs,
+                                  train_loader, hyps, device='cuda:1')
+
+    trained_model = ConvAutoEncoderShallow(69*69*3, mode='pnf')
+    trained_model.load_state_dict(torch.load(save_dir + '/mnsanitycheck-bs256-lr0.1.pt'))
+    trained_model.to('cuda:1')
+
+    to_pil_image = transforms.ToPILImage()
+
+    # test autoencoder:
+    gen_num = 10
+    counter = 0
+    seed = np.random.randint(0, 255)
+    for tensor, _ in valid_loader:
+        if counter >= gen_num:
+            break
+
+        tensor = tensor.to('cuda:1')
+        reconst_tensor = trained_model(tensor)
+        # ensure the directories exist
+        create_dir(dest)
+        image_dest = dest + f'/r-{counter}.png'
+        recon_dest = dest + f'/p-{counter}.png'
+
+        tensor = tensor.cpu()[seed]
+        reconst_tensor = reconst_tensor.cpu()[seed]
+
+        print(f'-----------> {tensor.shape} -----> {type(tensor)}')
+        real_img = to_pil_image(tensor)
+        pred_img = to_pil_image(reconst_tensor)
+
+        real_img = real_img.resize((690, 690))
+        pred_img = pred_img.resize((690, 690))
+
+        real_img.save(image_dest)
+        pred_img.save(recon_dest)
+
+        counter += 1
